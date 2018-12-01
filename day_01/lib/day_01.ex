39bb74b27e0d5c89d686bfd8032ff82818cb4631
Wil Chandler <wilchandler2@gmail.com>
2018-12-01 15:42:11 -0600

    [day 01] implements working version of part two

--- day_01/lib/day_01.ex
+++ day_01/lib/day_01.ex
@@ -10,4 +10,18 @@ defmodule AdventOfCode.Day01 do
   defp part_one([head | tail], acc) do
     part_one(tail, acc + head)
   end
+
+  def part_two(inputs) do
+    part_two(inputs, 0, MapSet.new([0]))
+  end
+
+  defp part_two([head | tail], acc, previous_values) do
+    value = part_one([head], acc)
+    if MapSet.member?(previous_values, value) do
+      value
+    else
+      new_set = MapSet.put(previous_values, value)
+      part_two(tail ++ [head], value, new_set)
+    end
+  end
 end
--- day_01/test/day_01_test.exs
+++ day_01/test/day_01_test.exs
@@ -40,4 +40,38 @@ defmodule AdventOfCode.Day01Test do
       assert AdventOfCode.Day01.part_one(inputs) == 569
     end
   end
+
+  describe "part two" do
+
+    test "first example can be calculated" do
+      inputs = [1, -1]
+
+      assert AdventOfCode.Day01.part_two(inputs) == 0
+    end
+
+
+    test "second example can be calculated" do
+      inputs = [3, 3, 4, -2, -4]
+
+      assert AdventOfCode.Day01.part_two(inputs) == 10
+    end
+
+    test "third example can be calculated" do
+      inputs = [-6, 3, 8, 5, -6]
+
+      assert AdventOfCode.Day01.part_two(inputs) == 5
+    end
+
+    test "fourth example can be calculated" do
+      inputs = [7, 7, -2, -7, -4]
+
+      assert AdventOfCode.Day01.part_two(inputs) == 14
+    end
+
+    test "puzzle input can be calculated" do
+      inputs = get_inputs_from_file("./data/part_one_puzzle_inputs.txt")
+
+      assert AdventOfCode.Day01.part_two(inputs) == 77666
+    end
+  end
 end
