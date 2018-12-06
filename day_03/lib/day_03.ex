4de187532c05eba6a1544a2b0161e3c836a0d9bf
Wil Chandler <wilchandler2@gmail.com>
2018-12-05 21:05:40 -0600

    [day 03] implements working version for part two

--- day_03/lib/day_03.ex
+++ day_03/lib/day_03.ex
@@ -1,17 +1,25 @@
 defmodule AdventOfCode.Day03 do
   alias AdventOfCode.Day03.Claim
 
-  def part_one(claims) do
+  require IEx
+
+  def overlapping_inches(claims) do
     build_piece(claims)
     |> apply_claims(claims)
     |> count_overlapping_claims
+  end
+
+  def independent_claims(claims) do
+    build_piece(claims)
+    |> apply_claims(claims)
+    |> filter_independent_claims
   end
 
   defp build_piece(claims) do
     x_size = Enum.map(claims, &(Claim.x_size(&1))) |> Enum.max
     y_size = Enum.map(claims, &(Claim.y_size(&1))) |> Enum.max
     rows = for _ <- (1..y_size), into: [] do
-      row_list = for _ <- (1..x_size), into: [], do: 0
+      row_list = for _ <- (1..x_size), into: [], do: []
       List.to_tuple(row_list)
     end
     List.to_tuple(rows)
@@ -30,7 +38,7 @@ defmodule AdventOfCode.Day03 do
         col_pos = (x + claim.x - 1)
         row = elem(acc2, row_pos)
         value = elem(row, col_pos)
-        new_row = put_elem(row, col_pos, value + 1)
+        new_row = put_elem(row, col_pos, [claim.id | value])
         put_elem(acc2, row_pos, new_row)
       end)
     end)
@@ -40,8 +48,49 @@ defmodule AdventOfCode.Day03 do
     Tuple.to_list(piece)
     |> Enum.reduce(0, fn(row, acc) ->
          row_count = Tuple.to_list(row)
-                     |> Enum.count(&(&1 > 1))
+                     |> Enum.count(&(length(&1) > 1))
          acc + row_count
        end)
   end
+
+  defp filter_independent_claims(piece) do
+    claims_per_inch =
+      Tuple.to_list(piece)
+      |> Enum.reduce([], fn(row, acc) ->
+        Tuple.to_list(row) ++ acc
+      end)
+
+    collect_ids_set(claims_per_inch)
+    |> reject_overlapping_claims(claims_per_inch)
+    |> MapSet.to_list
+  end
+
+  defp collect_ids_set(claims_per_inch) do
+    collect_ids_set(%MapSet{}, claims_per_inch)
+  end
+
+  defp collect_ids_set(set, []), do: set
+  defp collect_ids_set(set, [head_inch | tail]) do
+    list_into_set(set, head_inch)
+    |> collect_ids_set(tail)
+  end
+
+  defp list_into_set(set, []), do: set
+  defp list_into_set(set, [head | tail]) do
+    MapSet.put(set, head)
+    |> list_into_set(tail)
+  end
+
+  defp reject_overlapping_claims(set, []), do: set
+  defp reject_overlapping_claims(set, [head_inch | tail]) do
+    delete_list_elements_from_set(set, head_inch)
+    |> reject_overlapping_claims(tail)
+  end
+
+  defp delete_list_elements_from_set(set, []), do: set
+  defp delete_list_elements_from_set(set, [_ | []]), do: set
+  defp delete_list_elements_from_set(set, list) do
+    Enum.reduce(list, set, fn(el, acc) -> MapSet.delete(acc, el) end)
+  end
+
 end
--- day_03/test/day_03_test.exs
+++ day_03/test/day_03_test.exs
@@ -14,7 +14,7 @@ defmodule AdventOfCode.Day03Test do
     Enum.map(strings, &(Claim.from_string(&1)))
   end
 
-  describe "part_one" do
+  describe "overlapping_inches" do
     test "solves the example inputs" do
       raw_claims = [
         "#1 @ 1,3: 4x4",
@@ -23,15 +23,38 @@ defmodule AdventOfCode.Day03Test do
       ]
       claims = strings_to_claims(raw_claims)
 
-      assert Day03.part_one(claims) == 4
+      assert Day03.overlapping_inches(claims) == 4
     end
 
+    # @tag :skip
     test "solves the puzzle inputs" do
       claims =
         load_claims_from_file("./data/part_one_inputs.txt")
         |> strings_to_claims
 
-      assert Day03.part_one(claims) == 115304
+      assert Day03.overlapping_inches(claims) == 115304
+    end
+  end
+
+  describe "independent_claims" do
+    test "solves the example inputs" do
+      raw_claims = [
+        "#1 @ 1,3: 4x4",
+        "#2 @ 3,1: 4x4",
+        "#3 @ 5,5: 2x2"
+      ]
+      claims = strings_to_claims(raw_claims)
+
+      assert Day03.independent_claims(claims) == [3]
+    end
+
+    # @tag :skip
+    test "solves the puzzle inputs" do
+      claims =
+        load_claims_from_file("./data/part_one_inputs.txt")
+        |> strings_to_claims
+
+      assert Day03.independent_claims(claims) == [3]
     end
   end
 end
